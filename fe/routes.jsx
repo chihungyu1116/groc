6cbe5c690a8037a6393631d98cc23f3ffa6f0716
Chihung Yu <cyu0@walmartlabs.com>
2016-04-01 13:18:10 -0700

    add jerry stylesheet

--- fe/routes.jsx
+++ fe/routes.jsx
@@ -8,12 +8,10 @@ import {
 import AppContainer from './containers/AppContainer';
 import GroceryHelperContainer from './containers/GroceryHelperContainer';
 import ShoppingListContainer from './containers/ShoppingListContainer';
-import OnboardingContainer from './containers/OnboardingContainer';
 
 export default (
   <Route component={ AppContainer } path='/'>
     <Route path='/grocery-helper' component={ GroceryHelperContainer }></Route>
     <Route path='/shopping-list' component={ ShoppingListContainer }></Route>
-    <Route path='/onboarding' component={ OnboardingContainer }></Route>
   </Route>
 );
