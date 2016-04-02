76b73645e3d1a9923c5b67a5313f458b84165e20
ylin3 <ylin@walmartlabs.com>
2016-04-02 00:24:32 -0700

    finished suggestion list and "added to cart" confirmation. Change items not implemented yet.

--- fe/containers/SuggestionListContainer.jsx
+++ fe/containers/SuggestionListContainer.jsx
@@ -4,7 +4,7 @@ import _ from 'lodash'
 
 const LIST_CLASS_PREFIX = 'yj-suggestion-list'
 const ITEM_CLASS_PREFIX = 'yj-suggestion-item'
-
+const MAX_COUNT = 20
 
 
 class SuggestionListContainer extends React.Component {
@@ -12,6 +12,7 @@ class SuggestionListContainer extends React.Component {
     super(props)
     this.updateSuggestionList = this.updateSuggestionList.bind(this)
     this.browseAlternativeItems = this.browseAlternativeItems.bind(this)
+    this.addToCart = this.addToCart.bind(this)
   }
 
   componentWillMount() {
@@ -19,7 +20,7 @@ class SuggestionListContainer extends React.Component {
   }
 
   setInitialState() {
-    this.setState({dirty: false})
+    this.setState({dirty: false, addedToCart: false})
   }
 
   componentWillReceiveProps(nextProps) {
@@ -38,7 +39,42 @@ class SuggestionListContainer extends React.Component {
     })
   }
 
+  renderSuggestionListSummary(items) {
+    const totalPrice = this.getTotalPrice(items)
+    const containerClasses = this.getListClasses('summary-container')
+    const addToCartButtonClasses = this.getListClasses('add-to-cart-button')
+    const cancelButtonClasses = this.getListClasses('cancel-button')
+    const priceContainerClasses = this.getItemClasses('price-container')
+    const priceTotalClasses = this.getItemClasses('price-total')
+    return (
+      <div className={containerClasses}>Total:
+        <div className={priceContainerClasses}>
+          <div className={priceTotalClasses}>${this.getTotalPrice(items)}</div>
+        </div>
+      </div>
+    )
+  }
+
+  renderSuggestionListActions() {
+    const containerClasses = this.getListClasses('summary-container')
+    const addToCartButtonClasses = this.getListClasses('add-to-cart-button')
+    const cancelButtonClasses = this.getListClasses('cancel-button')
+    return (
+      <div className={containerClasses}>
+        <button className={cancelButtonClasses}>Not This Time</button>
+        <button className={addToCartButtonClasses} onClick={this.addToCart}>Add to Cart</button>
+      </div>
+    )
+  }
+
+  getTotalPrice(items) {
+    return items.reduce((previous, item) => { 
+      return item.price.list * item.count + previous
+    }, 0).toFixed(2)
+  }
+
   getItemClasses(className, itemCardType) {
+    itemCardType = itemCardType ? itemCardType : 'tile'
     return [`${ITEM_CLASS_PREFIX}-${className}`, itemCardType].join(' ')
   }
 
@@ -61,24 +97,35 @@ class SuggestionListContainer extends React.Component {
     const containerClasses = this.getItemClasses('container', itemCardType)
     const nameClasses = this.getItemClasses('name', itemCardType)
     const imageClasses = this.getItemClasses('image', itemCardType)
-    const priceClasses = this.getItemClasses('price', itemCardType)
-    return item.not_found ? null : (
-       <div className={containerClasses}>
-         <img className={imageClasses} src={data.images.thumbnail} />
-         <div className={nameClasses}>{data.name}</div>
-         <div className={priceClasses}>{price ? `$${price.list}` : 'Price not available'}</div>
-         { this.renderAmountEdit(index, count, itemCardType) }
-       </div>
+    return (
+      <div className={containerClasses}>
+        <img className={imageClasses} src={data.images.thumbnail} />
+        <div className={nameClasses}>{data.name}</div>
+        { this.renderAmountEdit(index, count, itemCardType) }
+        { this.renderPrice(price.list, count) }
+      </div>
+    )
+  }
+
+  renderPrice(price, count) {
+    const containerClasses = this.getItemClasses('price-container')
+    const priceTotalClasses = this.getItemClasses('price-total')
+    const priceEachClasses = this.getItemClasses(`price-each ${count === 1 ? 'yj-hidden' : ''}`)
+    return price === undefined ? (<div className={containerClasses}>Price not available</div>) : (
+      <div className={containerClasses}>
+        <div className={priceTotalClasses}>{`$${(price * count).toFixed(2)}`}</div>
+        <div className={priceEachClasses}>{`$${price} each`}</div>
+      </div>
     )
   }
 
   renderAmountEdit(index, count, itemCardType) {
     if ( itemCardType !== 'tile' ) { return null }
-    const editClasses = this.getItemClasses('edit', itemCardType)
-    const editButtonClasses = this.getItemClasses('edit-button', itemCardType)
-    const changeButtonClasses = this.getItemClasses('change-button', itemCardType)
-    const countClasses = this.getItemClasses('edit-count', itemCardType)
-    const addOne = () => { this.updateSuggestionList(index, count+1) }
+    const editClasses = this.getItemClasses('edit')
+    const editButtonClasses = this.getItemClasses('edit-button')
+    const changeButtonClasses = this.getItemClasses('change-button')
+    const countClasses = this.getItemClasses('edit-count')
+    const addOne = () => { this.updateSuggestionList(index, Math.min(MAX_COUNT, count+1)) }
     const subtractOne = () => { this.updateSuggestionList(index, Math.max(0, count-1)) }
     const changeItem = (event) => { this.browseAlternativeItems(index) }
     return (
@@ -107,11 +154,18 @@ class SuggestionListContainer extends React.Component {
 
   }
 
-  getSuggestionList() {
-    return this.state.dirty ? this.state.suggestionList : this.props.suggestionList
+  addToCart() {
+    console.log('add to cart')
+    // does not really add to cart
+    this.setState({addedToCart: true})
   }
 
-  render() {
+  getSuggestionList() {
+    const list = this.state.dirty ? this.state.suggestionList : this.props.suggestionList
+    return list.filter((item) => { return !item.not_found })
+  }
+
+  renderSuggestionList() {
     const suggestionList = this.getSuggestionList()
     const listContainerClasses = this.getListClasses('container')
     const listTitleClasses = this.getListClasses('title')
@@ -120,8 +174,34 @@ class SuggestionListContainer extends React.Component {
       <div className={listContainerClasses}>
         <h3 className={listTitleClasses}>{title}</h3>
         {this.renderSuggestionItems(suggestionList, 'thumbxnail')}
+        {this.renderSuggestionListSummary(suggestionList)}
+        {this.renderSuggestionListActions()}
       </div>
-    );
+    )
+  }
+
+  renderAddToCartConfirmation() {
+    const listContainerClasses = this.getListClasses('container')
+    return (
+      <div className={listContainerClasses}>
+        <div className="yj-add-to-cart-container">
+          <div className="yj-add-to-cart-positioner">
+            <div className="yj-add-to-cart-message">Items have been added to cart</div>
+            <div className="yj-add-to-cart-button-list">
+              <button className="yj-view-cart-button">
+                View Cart <i className="fa fa-shopping-cart"></i>
+              </button>
+              <button className="yj-checkout-button">Check Out</button>
+            </div>
+          </div>
+        </div>
+      </div>      
+    )
+  }
+
+  render() {
+    const { addedToCart } = this.state
+    return addedToCart ? this.renderAddToCartConfirmation() : this.renderSuggestionList()
   }
 }
 
